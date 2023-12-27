36bef9b2c6b446aef18f83257f107ea3c986d014
leandrodsd <leandrodsd18@gmail.com>
2023-12-27 08:23:39 -0300

    in

---
+++ comecando-js/curso-js-boson/exp02/script.js
@@ -0,0 +1,17 @@
+/*let a = 20, b = 10;
+
+document.write("A soma é: " + (a + b) + "<br>");
+document.write("A subtração é: " + (a - b) + "<br>");
+document.write("A multiplicação é: " + (a * b) + "<br>");
+document.write("A divisão é: " + (a / b) + "<br>");
+document.write("O módulo é: " + (a % b) + "<br>");
+document.write("O incremento de a: " + (a++) + "<br>");
+document.write("O decremento de b: " + (--b) + "<br>");*/
+
+
+alert("Vamos calcular a area de um terreno!")
+let cump = prompt("Digite o cumprimento em metros");
+let larg = prompt("Digite a largura em metros");
+
+let area = cump * larg;
+document.write("A Area do terreno é: "+ area);
