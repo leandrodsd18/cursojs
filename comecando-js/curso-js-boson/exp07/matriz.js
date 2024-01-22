b80a203ddc31074a5753480fecd6093bb76ba18d
leandrodsd <leandrodsd18@gmail.com>
2024-01-22 11:01:54 -0300

    fim matriz

--- comecando-js/curso-js-boson/exp07/matriz.js
+++ comecando-js/curso-js-boson/exp07/matriz.js
@@ -28,3 +28,36 @@ for(L=0; L<=2; L++){
     }
 }
 alert(valores2);
+
+//Criando uma matriz em tempo de execução
+
+var LINHAS= prompt("Digite o numero de linhas da matriz:");
+var COLUNAS= prompt("Digite o numero de colunas da matriz: ");
+alert("Criar matriz de "+ LINHAS + " linhas por " + COLUNAS + " colunas");
+
+var MATRIZ2= [];
+    for(i=0; i<LINHAS; i++){
+        MATRIZ2[i] =[];
+        for(j=0; j<COLUNAS; j++){
+            MATRIZ2[i][j] ='';
+        }
+    }
+//inserindo valores na matriz em tempo de execução
+for(L=0; L<LINHAS; L++){
+    for(C=0; C<COLUNAS; C++){
+        MATRIZ2[L][C]= prompt("Digite um valor para inserir na matriz: ");
+    }
+}
+//Lendo os valores da matriz
+var VALORES3 ='';
+for(L=0; L<LINHAS; L++){
+    for(C=0; C<COLUNAS; C++){
+        if(C < COLUNAS -1){
+            VALORES3 += MATRIZ2[L][C] + "\t\t";
+        }
+        else{
+            VALORES3 += MATRIZ2[L][C] + "\n";
+        }
+    }
+}
+alert("Conteúdo da matriz: \n"+ VALORES3);
