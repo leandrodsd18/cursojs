630082f5c05d4efdd1f8e65627551fafe899f7e1
leandrodsd <leandrodsd18@gmail.com>
2024-01-24 10:09:05 -0300

    Math

---
+++ comecando-js/curso-js-boson/exp09/objetoMath.js
@@ -0,0 +1,10 @@
+document.write("Logartimo natural de 10: " + Math.LN10 + "<br/>");
+document.write("O valor de PI é: " + Math.PI + "<br/>");
+document.write("A raiz quadrada de 2 é: " + Math.SQRT2 + "<br/>");
+
+let a = 3.25, x = 4, y = 2;
+
+document.write("X elevado a Y é: " +Math.pow(x,y)+ "<br/>");
+document.write("Arredondando para cima: " +Math.ceil(a) + "<br/>");
+document.write("Arredondando para baixo: " +Math.floor(a)+ "<br/>");
+document.write(Math.cos(x));
