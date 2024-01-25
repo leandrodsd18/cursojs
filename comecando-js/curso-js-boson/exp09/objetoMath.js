088e023763222c53e8354ae461ab52619e8770fb
leandrodsd <leandrodsd18@gmail.com>
2024-01-25 10:08:34 -0300

    date

--- comecando-js/curso-js-boson/exp09/objetoMath.js
+++ comecando-js/curso-js-boson/exp09/objetoMath.js
@@ -1,7 +1,9 @@
+//Algumas Propriedades do objeto Math
 document.write("Logartimo natural de 10: " + Math.LN10 + "<br/>");
 document.write("O valor de PI é: " + Math.PI + "<br/>");
 document.write("A raiz quadrada de 2 é: " + Math.SQRT2 + "<br/>");
 
+//Alguns Métodos do objeto Math
 let a = 3.25, x = 4, y = 2;
 
 document.write("X elevado a Y é: " +Math.pow(x,y)+ "<br/>");
---
+++ comecando-js/curso-js-boson/exp10/objetoDate.js
@@ -0,0 +1,8 @@
+//Exemplos de criação de datas:
+var hoje = new Date(); //Data e hora atuais
+var novaData = new Date(2024,3,26); //Atribuindo data
+var milissegundos = new Date(1706184074443);
+var dataString = new Date("2024/01/25, 8:57:55");
+document.write("Data: " + milissegundos , "<br/>");
+document.write("Data em milissegundos: ", Date.now(), "<br/>");
+
