b7bf1728e1231e62000fbda09dc9b92ab30df2d1
Leandro Daniel <leandrodsd18@gmail.com>
2024-05-17 08:57:31 -0300

    conclusão

--- curso-em-video/ex007/script.js
+++ curso-em-video/ex007/script.js
@@ -25,6 +25,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     }else{
         alert('Numero invalido ou ja encontrado na lista')
     }
@@ -32,3 +33,31 @@ function adicionar(){
     num.focus()
 }
 
+function finalizar(){
+    if(valores.length ==0){
+        alert('Adicione valores antes de finalizar')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior =valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informando foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os números é ${soma}</p>`
+        res.innerHTML += `<p>A média desses números é ${media}</p>`
+    }
+}
