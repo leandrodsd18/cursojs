5330bc64a0b1a5941e32f2308a0ceeb3fd7ba9bd
Leandro Daniel <leandrodsd18@gmail.com>
2024-06-06 09:46:11 -0300

    ex.

--- comecando-js/calculadora-geometrica/script.js
+++ comecando-js/calculadora-geometrica/script.js
@@ -26,5 +26,46 @@ function circulo(){
     const raio = parseFloat(prompt('Digite o valor do raio do circulo'))
     return 3.14 * (raio*raio)
 }
-let resultado = circulo()
-alert(resultado)
+
+function exibirMenu(){
+    return prompt('Escolha a área que deseja calcular:' +
+        '\n1- Área do Triângulo'+
+        '\n2- Área do Retângulo'+
+        '\n3- Área do Quadrado'+
+        '\n4- Área do Trapézio'+
+        '\n5- Área do Circulo'+
+        '\n6- Sair '
+    )
+}
+
+function executar(){
+    let opcao = ''
+
+    do{
+        opcao = exibirMenu()
+        switch(opcao){
+            case '1':
+                alert('A área do triângulo é: '+ triangulo())
+                break                
+            case '2':
+                alert('A área do retângulo é: '+ retangulo())
+                break
+            case '3':
+                alert('A área do quadrado é: '+ quadrado())
+                break
+            case '4':
+                alert('A área do trapézio é: '+ trapezio())
+                break
+            case '5':
+                alert('A área do circulo é: '+ circulo())
+                break
+            case '6':
+                alert('Saindo...')
+                break
+            default:
+                alert('Opção invalida! Selecione uma opção e tente novamente')
+        }
+
+    }while(opcao !== '6')
+}
+executar()
