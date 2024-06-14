a96846d8bcd04da3d6ae44322e1d2c54abd71022
Leandro Daniel <leandrodsd18@gmail.com>
2024-06-14 09:19:43 -0300

    exerc

--- comecando-js/vagas-emprego/index.js
+++ comecando-js/vagas-emprego/index.js
@@ -17,10 +17,26 @@ function novaVaga(){
     const dataLimite = prompt('Informe a data limite para a vaga: (dd/mm/aaaa)')
 
     const confirmacao = confirm('Deseja criar uma nova vaga com essas informações?\n' + 'Nome: ' + nome + '\nDescrição: ' + descricao + '\nData limite: '+ dataLimite)
-
+//Caso a confirmção seja true, então a nova vaga é criada!
     if(confirmacao){
         const novaVaga = {nome, descricao, dataLimite, candidatos:[]}
         vagas.push(novaVaga)
         alert('Vaga criada com sucesso!')
     }
+}
+//Função para exibir as vagas na tela!
+function exibirVaga(){
+    const indice = prompt('Informe o indice da vaga que deseja exibir:')
+    const vaga = vagas[indice]
+
+    const candidatosEmTexto = vaga.candidatos.reduce(function (textoFinal, candidato){ textoFinal + '\n -' + candidato, ''})
+
+    alert(
+        'Vaga n° ' + indice +
+        '\nNome: ' + vaga.nome +
+        '\nDescrição: ' + vaga.descricao +
+        '\nData Limite: ' + vaga.dataLimite +
+        '\nQuantidade de candidatos: ' + vaga.candidatos.length +
+        '\nCandidatos inscritos: ' + candidatosEmTexto
+    )
 }
