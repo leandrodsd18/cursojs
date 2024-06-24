2514a46b137f3bfb9132182c156e4a413c574ae3
Leandro Daniel <leandrodsd18@gmail.com>
2024-06-24 08:25:56 -0300

    atl nome arq

--- comecando-js/eventos-dom/enviandoValores.js
+++ comecando-js/eventos-dom/enviandoValores.js
@@ -1,3 +1,6 @@
+/*O parâmetro 'element' está ligado ao parâmetro 'this.parentNode' la no HTML, ao linkar o 'this.parentNode' com o 'element' estamos pegando todos os valores que estão nos inputs da <section> onde o <button> register está inserido*/ 
+
+/*a propriedade children no JavaScript retorna uma coleção com todos os elementos filhos do elemento HTML pelo qual foi chamado. */
 function register(element){
     const userName = element.children.userName.value
     const password = element.children.password.value
