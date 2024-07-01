032fc9664df8e830cf673a846c5b6f266678088e
Leandro Daniel <leandrodsd18@gmail.com>
2024-07-01 11:31:04 -0300

    ex

--- comecando-js/eventos-dom/criarEventos.js
+++ comecando-js/eventos-dom/criarEventos.js
@@ -1,1 +1,18 @@
-const button = document.getElementById('register-button')
+function register(ev){
+    console.log(ev)
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+    if(password === passwordConfirmation){
+        alert('Usuario(a)' + username +  'criado com sucesso!')
+    }
+    else{
+        alert('As senhas são diferentes')
+    }
+}
+
+const button = document.getElementById("register-button")
+
+button.addEventListener("click", register)
