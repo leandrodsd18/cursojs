57e5981a22be7601c465f330546ffcdc57285dc6
Leandro Daniel <leandrodsd18@gmail.com>
2024-07-02 09:28:51 -0300

    exercicios

--- exercicios/calculo-viagens/calculo.js
+++ exercicios/calculo-viagens/calculo.js
@@ -1,8 +1,16 @@
-function elevar(x, n){
-    if(n===0){
-        return 1
-    }else{
-        return x * elevar(x,n - 1)
+function elevarAoQuadrado(num){
+    //Variavel para armazenar o resultado final
+    let resultado = ''
+    //Primeiro converti o num para string para poder manipular cada algorismo separadamente
+    let numString = num.toString()
+    //Percorre cada algorismo da string
+    for(let i =0; i < numString.length; i++){
+        //Eleva cada um dos algorismos ao quadrado, e converte de volta para um valor numérico
+        const algorismoAoQuadrado = Math.pow(parseInt(numString[i]), 2)
+        //Concatena o resultado ao string de resultado
+        resultado += algorismoAoQuadrado.toString()
     }
+    //Converte o resultado final para um unico inteiro
+    return parseInt(resultado)
 }
-console.log(elevar(15,4))
+console.log(elevarAoQuadrado(9))
