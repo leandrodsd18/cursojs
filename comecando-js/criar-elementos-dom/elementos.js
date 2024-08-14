94188abe52cbc802216fe575aaf26b514dfbdafe
Leandro Daniel <leandrodsd18@gmail.com>
2024-08-14 09:07:08 -0300

    revisão

--- comecando-js/criar-elementos-dom/elementos.js
+++ comecando-js/criar-elementos-dom/elementos.js
@@ -3,10 +3,8 @@ Criei uma função que é chamada ao clicar no botão 'Adicionar input da pagina
 function addInput(){
     //Como não sei o número exato de inputs que serão adicionados, primeiro criei uma lista para receber todos os inputs
     const ul = document.getElementById('inputs')
-    //Cada input ficará dentro de uma 'li'
-    const newLi = document.createElement('li')
-    const label = document.createElement('label')
-    label.htmlFor = ('newInput')
+    //Cada input ficará dentro de uma 'li'    
+    const newLi = document.createElement('li')    
     newLi.id = 'newInput'
     newLi.className = 'list-item'
     newLi.innerText = 'Novo input: '
--- comecando-js/escalar-time/escalacao.js
+++ comecando-js/escalar-time/escalacao.js
@@ -1,4 +1,6 @@
-function addplayer(){
+/*Programa para escalar um time.
+Criei a função 'addplayer para escalar os jogadores do time. Nessa função o usuario vai entrar com a posição o nome e o número de cada jogador, antes de escalar o jogador o programa ainda pergunta se o usuario quer memso escalar tal jogador, após a confirmação o jogador é adicinado a uma lista. Podemos tbm remover um determinado jogador apenas pelo némero da camisa através da função 'removeToPlayer*/
+function addPlayer(){
     const position = document.getElementById('position').value 
     const name = document.getElementById('name').value
     const number = document.getElementById('number').value
