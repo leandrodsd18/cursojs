ffbfd9b8b9e559e8d4517f672603c43668fc65ae
Leandro Daniel <leandrodsd18@gmail.com>
2024-08-26 09:06:40 -0300

    Manipulando estilos

---
+++ comecando-js/manipulandoEstilos/manipulandoEstilos.js
@@ -0,0 +1,18 @@
+function useLightTheme(){
+    document.body.style.color = '#212529'
+    document.body.style.backgroundColor = '#f1f5f9'
+}
+
+function useDarkTheme(){
+    document.body.style.color = '#f1f5f9'
+    document.body.style.backgroundColor = '#212529'
+}
+
+function useSwitchTheme(){
+    document.body.classList.toggle('is-ligth')
+    document.body.classList.toggle('is-dark')
+}
+
+document.getElementById('lightBtn').addEventListener('click', useLightTheme)
+document.getElementById('darkBtn').addEventListener('click', useDarkTheme)
+document.getElementById('switchBtn').addEventListener('click', useSwitchTheme)
